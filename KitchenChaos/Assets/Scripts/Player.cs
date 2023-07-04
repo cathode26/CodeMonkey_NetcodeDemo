fb242e43ae3a084d80bfea110cebd92ac39cbc11
cathode26 <Vincent.J.Liguori@outlook.com>
2023-07-03 23:34:39 -0400

    Added Player script with movement and rotation
    Introduced a new script to handle player movement and rotation. The script controls the players speed and direction based on the keyboard input.  There is a direction based speed adjustment that makes movement more realistic.
    1) Controls the player with wasd
    2) Smooth rotation: The player's rotation smoothly transitions towards the direction of movement using spherical linear interpolation (Slerp).
    3) Direction-based speed adjustment: The player's speed varies between 0 and full speed based on the angle difference between the player's forward direction and the movement direction. The larger the angle, the slower the player moves.

---
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // This script should be attached to an empty root node.
+    // The actual player model/mesh should be a child of the root node.
+    // This allows you to scale the model independently, while only moving and rotating the root.
+    //Private, public, protected are called the accessors
+    [SerializeField]
+    private float movementSpeed = 5.0f;
+    [SerializeField]
+    private float rotationSpeed = 5.0f;
+
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0,0);
+        bool moved = false;
+
+        // Check player input for movement and set the inputVector and moved accordingly
+        if (Input.GetKey(KeyCode.W))
+        {
+            moved = true;
+            inputVector.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moved = true; 
+            inputVector.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moved = true;
+            inputVector.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moved = true;
+            inputVector.x += 1;
+        }
+        if (moved)
+        {
+            // Normalize the input vector to ensure consistent movement speed in all directions
+            inputVector.Normalize();
+            Vector3 movDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+            //Rotate the forward vector, slowly by deltaTime
+            //Smoothly rotate the player to face the direction of movement using Spherical Linear Interpolation (Slerp)
+            transform.forward = Vector3.Slerp(transform.forward, movDir, Time.deltaTime * rotationSpeed);
+
+            // Calculate the difference in direction between where the player is currently facing (transform.forward)
+            // and the desired direction of movement (movDir)
+            float angleDifference = Vector3.Angle(transform.forward, movDir);
+            float normalizeDifference = angleDifference / 180.0f; // change the angle so it is between 0 and 1
+
+            // Adjust the player's speed based on the difference in direction.
+            // The larger the angleDifference, the slower the player moves.
+            float adjustedSpeed = Mathf.Lerp(0.0f, movementSpeed, 1 - normalizeDifference); 
+            transform.position += movDir * Time.deltaTime * adjustedSpeed;
+        }
+    }
+}
