9c99bd8fc4b48eed1cd69b6b590e80c5e84b58df
cathode26 <Vincent.J.Liguori@outlook.com>
2023-07-04 16:51:39 -0400

    Refactor Player movement and add collision detection
    - Refactored Player movement logic into separate functions for better modularity.
    - Implemented collision detection for Player movement.
    When a collision is detected, the algorithm will analyze each axis individually to find a viable direction that is not blocked.
    - Separated movement and rotation logic from the Player's Update method into a standalone function for better clarity and maintainability.

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -10,6 +10,10 @@ public class Player : MonoBehaviour
     private float movementSpeed = 5.0f;
     [SerializeField]
     private float rotationSpeed = 5.0f;
+    [SerializeField]
+    float playerRadius = 0.35f;
+    [SerializeField]
+    float playerHeight = 2.0f;
     [SerializeField] 
     private GameInput gameInput;
 
@@ -22,32 +26,20 @@ public class Player : MonoBehaviour
     private void Update()
     {
         (bool, Vector2) movementData = gameInput.GetMovementVectorNormalized();
-        Vector2 inputVector = movementData.Item2;
-        bool moved = movementData.Item1;
-
-        if (moved)
+        
+        if (movementData.Item1)
         {
-            // Normalize the input vector to ensure consistent movement speed in all directions
-            inputVector.Normalize();
-            Vector3 movDir = new Vector3(inputVector.x, 0f, inputVector.y);
+            (bool, Vector3) TryMoveData = DetermineMovementAbilityAndDirection(movementData.Item2);
+            bool canMove = TryMoveData.Item1;
+            Vector3 movDir = TryMoveData.Item2;
 
-            //Rotate the forward vector, slowly by deltaTime
-            //Smoothly rotate the player to face the direction of movement using Spherical Linear Interpolation (Slerp)
-            transform.forward = Vector3.Slerp(transform.forward, movDir, Time.deltaTime * rotationSpeed);
-
-            // Calculate the difference in direction between where the player is currently facing (transform.forward)
-            // and the desired direction of movement (movDir)
-            float angleDifference = Vector3.Angle(transform.forward, movDir);
-            float normalizeDifference = angleDifference / 180.0f; // change the angle so it is between 0 and 1
-
-            // Adjust the player's speed based on the difference in direction.
-            // The larger the angleDifference, the slower the player moves.
-            float adjustedSpeed = Mathf.Lerp(0.0f, movementSpeed, 1 - normalizeDifference); 
-            transform.position += movDir * Time.deltaTime * adjustedSpeed;
-            if (IsWalking == false)
-                OnWalkingStateChanged(true);
-            IsWalking = true;
-
+            if (canMove)
+            {
+                MoveAndRotatePlayer(movDir);
+                if (IsWalking == false)
+                    OnWalkingStateChanged(true);
+                IsWalking = true;
+            }
         }
         else
         {
@@ -55,5 +47,58 @@ public class Player : MonoBehaviour
                 OnWalkingStateChanged(false);
             IsWalking = false;
         }
+    }
+
+    /* 
+     * Input: the input vector from unity's new input system
+     * Function will rotate the players transform and move the players transform by the movDir
+     */
+    private void MoveAndRotatePlayer(Vector3 movDir)
+    {
+        //Rotate the forward vector, slowly by deltaTime
+        //Smoothly rotate the player to face the direction of movement using Spherical Linear Interpolation (Slerp)
+        transform.forward = Vector3.Slerp(transform.forward, movDir, Time.deltaTime * rotationSpeed);
+
+        // Calculate the difference in direction between where the player is currently facing (transform.forward)
+        // and the desired direction of movement (movDir)
+        float angleDifference = Vector3.Angle(transform.forward, movDir);
+        float normalizeDifference = angleDifference / 180.0f; // change the angle so it is between 0 and 1
+
+        // Adjust the player's speed based on the difference in direction.
+        // The larger the angleDifference, the slower the player moves.
+        float adjustedSpeed = Mathf.Lerp(0.0f, movementSpeed, 1 - normalizeDifference);
+        transform.position += movDir * Time.deltaTime * adjustedSpeed;
+    }
+
+    /* 
+     * Input: the input vector from unity's new input system
+     * Output: a tuple containing a bool indicating if the player can move, and a Vector3 that represents the validated movement in 3d
+     * 
+     * This function will attempt to find a valid direction that the player can travel by 
+     * 1) Creating a Capsule around the player
+     * 2) Checking if the capsule when moved in the direction of the input vector, collides with any game object 
+     * If the player can't move in the desired direction, the function tries alternative directions by considering each axis individually.
+     * It does this by modifying the 3D representation of the input vector, effectively "removing" the axis that caused the collision.
+     * This way, the function validates the movement direction and also handles the conversion of the 2D input vector to a 3D vector.
+     */
+    private (bool, Vector3) DetermineMovementAbilityAndDirection(Vector2 inputVector)
+    {
+        Vector3 movDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        float moveDistance = movementSpeed * Time.deltaTime;
+
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movDir, moveDistance);
+
+        if (!canMove && movDir.x != 0.0f && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, new Vector3(movDir.x, 0, 0), moveDistance))
+        {
+            canMove = true;
+            movDir.z = 0;
+        }
+        else if (!canMove && movDir.z != 0.0f && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, new Vector3(0, 0, movDir.z), moveDistance))
+        {
+            canMove = true;
+            movDir.x = 0;
+        }
+
+        return (canMove, movDir);
     }
 }
