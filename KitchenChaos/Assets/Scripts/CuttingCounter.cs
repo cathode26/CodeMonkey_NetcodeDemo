7de5bcd43c9c2feb5265a7e00c81167c365e2410
cathode26 <Vincent.J.Liguori@outlook.com>
2023-07-06 20:46:21 -0400

    Implement Cutting Progress Bar UI
    Added a CuttingProgressBarUI class. This class handles the visual representation of the progress bar UI while the player is cutting a KitchenObject. It uses an Image component to display the cutting progress and listens to relevant events from the CuttingCounter class.
    
    Enhanced the CuttingCounter class to emit more granular events during the cutting process, such as when a cuttable object is set, when an object is removed, and when the player performs a cut. It keeps track of the current cutting progress and calculates the percent cut based on the required cuts defined in the CuttingRecipeSO.
    
    The CuttingCounter now also checks if a KitchenObject is cuttable by checking if it matches any of the input objects in the provided CuttingRecipeSO array.
    
    Finally, an enhancement has been made to the InteractAlternative method in the CuttingCounter class. It now checks the cutting progress and replaces the original object with a cut version once the cutting progress reaches the required amount.

--- KitchenChaos/Assets/Scripts/CuttingCounter.cs
+++ KitchenChaos/Assets/Scripts/CuttingCounter.cs
@@ -7,8 +7,15 @@ public class CuttingCounter : BaseCounter
     [SerializeField]
     private CuttingRecipeSO[] cuttingRecipesSO;
 
-    public event EventHandler OnPlayerCutObject;
+    public event EventHandler OnPlayerSetCuttableObject;
+    public event EventHandler OnPlayerRemovedObject;
+    public event EventHandler<OnPlayerCutEventArgs> OnPlayerCutObject;
     private int cuttingProgress = 0;
+
+    public class OnPlayerCutEventArgs : EventArgs
+    {
+        public float percentCut;
+    }
 
     public override void Interact(Player player)
     {
@@ -17,20 +24,24 @@ public class CuttingCounter : BaseCounter
             KitchenObject kitchenObject = player.GetKitchenObject();
             kitchenObject.SetKitchenObjectsParent(this);
             cuttingProgress = 0;
+
+            if (IsKitchenObjectCuttable(kitchenObject))
+                OnPlayerSetCuttableObject?.Invoke(this, EventArgs.Empty);
         }
         else if (HasKitchenObject() && !player.HasKitchenObject())
         {
             KitchenObject kitchenObject = GetKitchenObject();
             kitchenObject.SetKitchenObjectsParent(player);
+            OnPlayerRemovedObject?.Invoke(this, EventArgs.Empty);
         }
     }
-    public override void InteractAlternative(Player player) 
+    public override void InteractAlternative(Player player)
     {
         if (HasKitchenObject())
         {
             //Lets start by destroying the food when we cut it
             KitchenObject kitchenObject = GetKitchenObject();
-            
+
             //determine which cut scriptable object to create with the map
             KitchenObjectSO uncutKitchenObjectSO = kitchenObject.GetKitchenObjectSO();
             if (uncutKitchenObjectSO)
@@ -38,9 +49,10 @@ public class CuttingCounter : BaseCounter
                 CuttingRecipeSO cuttingRecipeSO = cuttingRecipesSO.FirstOrDefault(uncut => uncut.input == uncutKitchenObjectSO);
                 if (cuttingRecipeSO)
                 {
-                    //animate the cutting
-                    OnPlayerCutObject?.Invoke(this, EventArgs.Empty);
                     cuttingProgress++;
+                    //animate the cutting
+                    OnPlayerCutObject?.Invoke(this, new OnPlayerCutEventArgs() { percentCut = (float)cuttingProgress / (float)cuttingRecipeSO.cuttingProgressMax });
+
                     KitchenObjectSO cutKitchenObjectSO = cuttingRecipeSO.output;
                     if (cutKitchenObjectSO && cuttingProgress >= cuttingRecipeSO.cuttingProgressMax)
                     {
@@ -50,5 +62,12 @@ public class CuttingCounter : BaseCounter
                 }
             }
         }
+    }
+
+    private bool IsKitchenObjectCuttable(KitchenObject kitchenObject)
+    {
+        KitchenObjectSO maybeUncutKitchenObjectSO = kitchenObject.GetKitchenObjectSO();
+        CuttingRecipeSO cuttingRecipeSO = cuttingRecipesSO.FirstOrDefault(uncut => uncut.input == maybeUncutKitchenObjectSO);
+        return cuttingRecipeSO != null;
     }
 }
