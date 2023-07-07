a7e53b297eeebbed23c5853f9388c990e3c7ef74
cathode26 <Vincent.J.Liguori@outlook.com>
2023-07-07 17:03:49 -0400

    Added PlatesCounter and PlatesCounterVisual classes
    Introduces a mechanism for spawning plates at a regular interval and displaying them on the countertop. The PlatesCounter class handles the spawning and interaction logic, while the PlatesCounterVisual class manages the visual representation of the plates. Event handlers are also introduced to handle the spawning and consumption of plates.

---
+++ KitchenChaos/Assets/Scripts/PlatesCounterVisual.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+// The PlatesCounterVisual class handles the visual aspect of the plates on the countertop.
+public class PlatesCounterVisual : MonoBehaviour
+{
+    [SerializeField] private PlatesCounter platesCounter;
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private Transform plateVisualPrefab;
+    [SerializeField] private float plateOffset = 0.1f;
+
+    // A list of game objects that represent the plates on the countertop.
+    private List<GameObject> plateVisualGameObjectList = new List<GameObject>();
+
+    // Subscribe to the plate-related events.
+    private void OnEnable()
+    {
+        platesCounter.OnPlateSpawned += PlatesCounter_OnPlateSpawned;
+        platesCounter.OnPlateConsumed += PlatesCounter_OnPlateConsumed;
+    }
+
+    // Unsubscribe from the plate-related events.
+    private void OnDisable()
+    {
+        platesCounter.OnPlateSpawned -= PlatesCounter_OnPlateSpawned;
+        platesCounter.OnPlateConsumed -= PlatesCounter_OnPlateConsumed;
+    }
+
+    // This method is invoked when a plate is spawned.
+    // It creates a visual representation of a plate at a specific position.
+    private void PlatesCounter_OnPlateSpawned(object sender, System.EventArgs e)
+    {
+        Transform plate = Instantiate(plateVisualPrefab, counterTopPoint);
+        plate.transform.localPosition = Vector3.zero;
+        plate.transform.localPosition = new Vector3(0, plateOffset * plateVisualGameObjectList.Count, 0);
+        plateVisualGameObjectList.Add(plate.gameObject);
+    }
+
+    // This method is invoked when a plate is consumed.
+    // It destroys the visual representation of the last spawned plate.
+    private void PlatesCounter_OnPlateConsumed(object sender, System.EventArgs e)
+    {
+        GameObject plate = plateVisualGameObjectList.Last();
+        plateVisualGameObjectList.Remove(plate);
+        Destroy(plate);
+    }
+}
