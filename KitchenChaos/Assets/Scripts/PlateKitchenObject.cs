7b3b7862c509bb8b695a4097f3f4aee0616ba0b6
cathode26 <Vincent.J.Liguori@outlook.com>
2023-07-10 17:24:10 -0400

    Added Multiple Recipes
    Changed PlateKitchenObject so that multiple recipes can be made.

--- KitchenChaos/Assets/Scripts/PlateKitchenObject.cs
+++ KitchenChaos/Assets/Scripts/PlateKitchenObject.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class PlateKitchenObject : KitchenObject
 {
     [SerializeField]
-    private PlatingRecipeSO platingRecipeSO;  // The recipe associated with this plate
+    private PlatingRecipeSO[] platingRecipeSOs;  // The recipe associated with this plate
     [SerializeField]
     private Transform plateTopPoint;  // The top point of the plate where ingredients are visually added
-    HashSet<KitchenObjectSO> platedFood = new HashSet<KitchenObjectSO>();  // A set of all food added to this plate
+    HashSet<KitchenObjectSO> platedFoods = new HashSet<KitchenObjectSO>();  // A set of all food added to this plate
 
     public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAddedEvent;
     public class OnIngredientAddedEventArgs : EventArgs
@@ -23,11 +23,11 @@ public class PlateKitchenObject : KitchenObject
     public bool TryAddIngredient(KitchenObject ingredient)
     {
         // If the ingredient isn't part of the recipe or has already been added, do nothing
-        if (!IsPartOfRecipe(ingredient) || platedFood.Contains(ingredient.GetKitchenObjectSO()))
+        if (!IsPartOfRecipe(ingredient) || platedFoods.Contains(ingredient.GetKitchenObjectSO()))
             return false;
 
         // Add the ingredient to the plate and destroy the ingredient object
-        platedFood.Add(ingredient.GetKitchenObjectSO());
+        platedFoods.Add(ingredient.GetKitchenObjectSO());
         ingredient.DestroySelf();
 
         OnIngredientAddedEvent.Invoke(this, new OnIngredientAddedEventArgs() { kitchenObjectSO = ingredient.GetKitchenObjectSO() });
@@ -38,8 +38,16 @@ public class PlateKitchenObject : KitchenObject
     // Check if a kitchen object is part of the associated recipe
     private bool IsPartOfRecipe(KitchenObject kitchenObject)
     {
-        KitchenObjectSO addItemSO = kitchenObject.GetKitchenObjectSO();
-        KitchenObjectSO matched = platingRecipeSO.input.FirstOrDefault(recipeItem => recipeItem == addItemSO);
-        return matched != null;
+        //Loop through the platingRecipeSOs and create and filter out all of the PlatingRecipeSO list that dont contain all of the platedFoods
+        List<PlatingRecipeSO> recipeSOs = new List<PlatingRecipeSO>();
+        foreach (PlatingRecipeSO platingRecipeSO in platingRecipeSOs)
+        {
+            if (!platingRecipeSO.input.Contains(kitchenObject.GetKitchenObjectSO()))
+                continue;
+
+            if (platedFoods.All(food => platingRecipeSO.input.Contains(food)))
+                return true;
+        }
+        return false;
     }
 }
